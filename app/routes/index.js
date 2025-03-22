a00694d461fc119b9e7550559c266135976d1295
Ganesh Rajan <ganesh.nr@frequence.com>
2025-03-22 10:05:11 +0530

    add environment file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: `${process.env.ENVIRONMENT} Express` });
 });
 
 module.exports = router;
